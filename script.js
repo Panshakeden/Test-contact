595c37ecc2b8614ed5506b7c7583c44b97217461
panshak <panshakeden@gmail.com>
2023-12-16 11:53:39 +0100

    getting id

---
+++ script.js
@@ -0,0 +1,5 @@
+const ContactForm= document.getElementById('contactForm');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const phoneInput = document.getElementById('phone');
+const contactList = document.getElementById('contactList');
