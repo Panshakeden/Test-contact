1a2432ef9d4a2fee87ef92a07e448db05eb84d9e
panshak <panshakeden@gmail.com>
2023-12-16 12:16:01 +0100

    creating li

--- script.js
+++ script.js
@@ -29,3 +29,22 @@ if (name && email && phone) {
 }
 });
 
+//display function
+function displayContacts(){
+    contactList.innerHTML=" ";
+    contacts.forEach((contact,index)=>{
+        const li= document.createElement('li');
+
+        li.innerHTML=`
+        <span>${contact.name}</span> <br>
+        <span>${contact.email}</span> <br>
+        <span>${contact.phone}</span>
+        
+        ///button for edit and delete
+        <button onclick="editContact(${index})">Edit</button>
+            <button onclick="deleteContact(${index})">Delete</button>
+    `;
+    contactList.appendChild(li)
+        });
+}
+
