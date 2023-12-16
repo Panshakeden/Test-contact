9179e88636d9cf58feb9e312f7b430cf4d2d069a
panshak <panshakeden@gmail.com>
2023-12-16 12:48:18 +0100

    final

--- script.js
+++ script.js
@@ -1,66 +1,54 @@
-//getting input id
-const contactForm= document.getElementById('contactForm');
+const contactForm = document.getElementById('contactForm');
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 const phoneInput = document.getElementById('phone');
 const contactList = document.getElementById('contactList');
- 
-//declearing as an array
-let contacts= [];
 
-contactForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
+let contacts = [];
 
-// getting contact value
-const name = nameInput.value;
-const email = emailInput.value;
-const phone = phoneInput.value;
+contactForm.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-// condition for validation
-if (name && email && phone) {
-    const newContact={name,email,phone}
-    contacts.push(newContact);
-   //creating a function to handle display of contact
-   displayContacts();
-    contactForm.reset();
-} else {
-    alert('Please kindly add all fields')
-    
-}
+    const name = nameInput.value;
+    const email = emailInput.value;
+    const phone = phoneInput.value;
+
+    if (name && email && phone) {
+        const newContact = { name, email, phone };
+        contacts.push(newContact);
+        displayContacts();
+        contactForm.reset();
+    } else {
+        alert('Please fill in all fields!');
+    }
 });
 
-//display function
-function displayContacts(){
-    contactList.innerHTML=" ";
-    contacts.forEach((contact,index)=>{
-        const li= document.createElement('li');
-
-        li.innerHTML=`
-        <span>${contact.name}</span> <br>
-        <span>${contact.email}</span> <br>
-        <span>${contact.phone}</span>
-        
-        ///button for edit and delete
-        <button onclick="editContact(${index})">Edit</button>
+function displayContacts() {
+    contactList.innerHTML = '';
+    contacts.forEach((contact, index) => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <span>${contact.name}</span>
+            <span>${contact.email}</span>
+            <span>${contact.phone}</span>
+            <button onclick="editContact(${index})">Edit</button>
             <button onclick="deleteContact(${index})">Delete</button>
-    `;
-    contactList.appendChild(li)
-        });
+        `;
+        contactList.appendChild(li);
+    });
 }
 
-//edit form function and display update
-function editContact(index){
-    const{name,email ,phone}=[index];
-    nameInput.value=name;
-    emailInput.value=email;
-    phoneInput.value=phone;
+function editContact(index) {
+    const { name, email, phone } = contacts[index];
+    nameInput.value = name;
+    emailInput.value = email;
+    phoneInput.value = phone;
 
-    contacts.splice(index,1);
+    contacts.splice(index, 1);
     displayContacts();
 }
 
-// delete contact and remove then display update
-function deleteContact(index){
-    contacts.splice(index,1);
+function deleteContact(index) {
+    contacts.splice(index, 1);
     displayContacts();
 }
