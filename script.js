63266916760e0802c2ed474a3f3a329f3c496c19
panshak <panshakeden@gmail.com>
2023-12-16 11:58:10 +0100

    adding evnt

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const ContactForm= document.getElementById('contactForm');
+//getting input id
+const contactForm= document.getElementById('contactForm');
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 const phoneInput = document.getElementById('phone');
 const contactList = document.getElementById('contactList');
+ 
+//declearing as an array
+let contacts= [];
+
+contactForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+// getting contact value
+})
